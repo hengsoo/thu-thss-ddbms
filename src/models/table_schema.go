dea086fca6dfb84368b9463e8986c45173fff67c
hengsoo <hengsoo.sjh@gmail.com>
2021-10-30 21:25:56 +0800

    feat: add helper functions to table_schema

--- src/models/table_schema.go
+++ src/models/table_schema.go
@@ -5,3 +5,27 @@ type TableSchema struct {
 	TableName     string
 	ColumnSchemas []ColumnSchema
 }
+
+// Get (column idx, column data type) by name
+func (schema *TableSchema) getColumnSchemaByName(colName string) (int, int) {
+	var colSchemas = schema.ColumnSchemas
+	for idx, col := range colSchemas {
+		if col.Name == colName {
+			return idx, col.DataType
+		}
+	}
+	// default
+	return -1, -1
+}
+
+// Get column idx by name
+func (schema *TableSchema) getColIndexByName(colName string) int {
+	colIdx, _ := schema.getColumnSchemaByName(colName)
+	return colIdx
+}
+
+// Get column data type by name
+func (schema *TableSchema) getColTypeByName(colName string) int {
+	_, colType := schema.getColumnSchemaByName(colName)
+	return colType
+}
