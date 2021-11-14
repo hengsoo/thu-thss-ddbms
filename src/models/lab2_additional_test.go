03a1bb5a14545e8951f05679de05028851209dac
hengsoo <hengsoo.sjh@gmail.com>
2021-11-15 00:11:27 +0800

    test: add additional test boilerplates

---
+++ src/models/lab2_additional_test.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"testing"
+)
+
+
+// Multiple Tables Join I
+func TestLab2Additional3TablesJoin(t *testing.T) {
+	// TODO
+	t.Fatal("Test not implemented")
+}
+
+// Multiple Tables Join II
+func TestLab2Additional5TablesJoin(t *testing.T) {
+	// TODO
+	t.Fatal("Test not implemented")
+}
+
+// TODO
+// Please add more relevant tests on Multiple Tables Join
