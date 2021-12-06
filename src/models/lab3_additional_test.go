640d946e061b835ec85cabe07692d06a94ee13b9
黄祖斌 <huang-zb18@mails.tsinghua.edu.cn>
2021-12-06 23:00:16 +0800

    test: add a new VF with replica join test

--- src/models/lab3_additional_test.go
+++ src/models/lab3_additional_test.go
@@ -82,3 +82,80 @@ func TestLab3AdditionalVerticalFragmentation(t *testing.T) {
 		t.Errorf("Incorrect join results, expected %v, actual %v", expectedDataset, results)
 	}
 }
+
+func TestLab3AdditionalVerticalFragmentation2(t *testing.T) {
+	setupLab3()
+
+	// use the client to create table and insert
+	// divide student table into two partitions and assign them to node0 and node1
+	m := map[string]interface{}{
+		"0|1|2": map[string]interface{}{
+			"predicate": map[string]interface{}{
+				"grade": [...]map[string]interface{}{{
+					"op":  "<=",
+					"val": 3.6,
+				},
+				},
+			},
+			"column": [...]string{
+				"sid", "name","age",
+			},
+		},
+		"0|1": map[string]interface{}{
+			"predicate": map[string]interface{}{
+				"grade": [...]map[string]interface{}{{
+					"op":  "<=",
+					"val": 3.6,
+				},
+				},
+			},
+			"column": [...]string{
+				"grade",
+			},
+		},
+		"0": map[string]interface{}{
+			"predicate": map[string]interface{}{
+				"grade": [...]map[string]interface{}{{
+					"op":  ">",
+					"val": 3.6,
+				},
+				},
+			},
+			"column": [...]string{
+				"sid", "name", "age", "grade",
+			},
+		},
+	}
+	studentTablePartitionRules, _ = json.Marshal(m)
+
+	// assign course registration to node2
+	m = map[string]interface{}{
+		"3": map[string]interface{}{
+			"predicate": map[string]interface{}{
+				"courseId": [...]map[string]interface{}{{
+					"op":  ">=",
+					"val": 0,
+				},
+				},
+			},
+			"column": [...]string{
+				"sid", "courseId",
+			},
+		},
+	}
+	courseRegistrationTablePartitionRules, _ = json.Marshal(m)
+
+	buildTablesLab3(cli)
+	insertDataLab3(cli)
+
+	// perform a join and check the result
+	results := Dataset{}
+	cli.Call("Cluster.Join", []string{studentTableName, courseRegistrationTableName}, &results)
+	expectedDataset := Dataset{
+		Schema: joinedTableSchema,
+		Rows:   joinedTableContent,
+	}
+	if !datasetDuplicateChecking(expectedDataset, results) {
+		t.Errorf("Incorrect join results, expected %v, actual %v", expectedDataset, results)
+	}
+}
