048a34bd3a8d2ac6ea0b3ba0501bcf76ae3ea4f5
黄祖斌 <huang-zb18@mails.tsinghua.edu.cn>
2021-12-07 19:59:52 +0800

    test: add set cover test

--- src/models/lab3_additional_test.go
+++ src/models/lab3_additional_test.go
@@ -151,6 +151,92 @@ func TestLab3AdditionalVerticalFragmentation2(t *testing.T) {
 	// perform a join and check the result
 	results := Dataset{}
 	cli.Call("Cluster.Join", []string{studentTableName, courseRegistrationTableName}, &results)
+
+
+	expectedDataset := Dataset{
+		Schema: joinedTableSchema,
+		Rows:   joinedTableContent,
+	}
+	if !datasetDuplicateChecking(expectedDataset, results) {
+		t.Errorf("Incorrect join results, expected %v, actual %v", expectedDataset, results)
+	}
+}
+
+func TestLab3AdditionalSetCover(t *testing.T) {
+	setupLab3()
+
+	// use the client to create table and insert
+	// divide student table into two partitions and assign them to node0 and node1
+	m := map[string]interface{}{
+		"0|1": map[string]interface{}{
+			"predicate": map[string]interface{}{
+				"grade": [...]map[string]interface{}{{
+					"op":  "<=",
+					"val": 3.6,
+				},
+				},
+			},
+			"column": [...]string{
+				"sid", "name","age",
+			},
+		},
+		"1|2": map[string]interface{}{
+			"predicate": map[string]interface{}{
+				"grade": [...]map[string]interface{}{{
+					"op":  "<=",
+					"val": 3.6,
+				},
+				},
+			},
+			"column": [...]string{
+				"grade",
+			},
+		},
+		"2": map[string]interface{}{
+			"predicate": map[string]interface{}{
+				"grade": [...]map[string]interface{}{{
+					"op":  ">",
+					"val": 3.6,
+				},
+				},
+			},
+			"column": [...]string{
+				"sid", "name", "age", "grade",
+			},
+		},
+	}
+	studentTablePartitionRules, _ = json.Marshal(m)
+
+	// assign course registration to node2
+	m = map[string]interface{}{
+		"3": map[string]interface{}{
+			"predicate": map[string]interface{}{
+				"courseId": [...]map[string]interface{}{{
+					"op":  ">=",
+					"val": 0,
+				},
+				},
+			},
+			"column": [...]string{
+				"sid", "courseId",
+			},
+		},
+	}
+	courseRegistrationTablePartitionRules, _ = json.Marshal(m)
+
+	buildTablesLab3(cli)
+	insertDataLab3(cli)
+
+	joinedTableContent = []Row{
+		{0, "John", 22, 4.0, 0},
+		{0, "John", 22, 4.0, 1},
+		{1, "Smith", 23, 3.6, 0},
+		{2, "Hana", 21, 4.0, 2},
+	}
+
+	// perform a join and check the result
+	results := Dataset{}
+	cli.Call("Cluster.Join", []string{studentTableName, courseRegistrationTableName}, &results)
 	expectedDataset := Dataset{
 		Schema: joinedTableSchema,
 		Rows:   joinedTableContent,
